fd9bae8de7d214988ef2686695165349616e17c2
Hanna Ilkiv <shov.guitar@gmail.com>
2026-03-16 16:18:37 +0100

    create  10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,95 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+
 console.log('timer');
+
+const startBtn = document.querySelector('button[data-start]');
+const datetimePicker = document.querySelector('#datetime-picker');
+const Ddays = document.querySelector('[data-days]');
+const Dhours = document.querySelector('[data-hours]');
+const Dminutes = document.querySelector('[data-minutes]');
+const Dseconds = document.querySelector('[data-seconds]');
+const timer = document.querySelector('.timer');
+
+
+let userSelectedDate = null;
+let timerId = null;
+
+startBtn.disabled = true;
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+      const selectedDate = selectedDates;
+      console.log(selectedDates[0]);
+      
+      if (selectedDate[0] < new Date()) {
+          alert("Please choose a date in the future");
+          startBtn.disabled = true;
+
+      } else {
+          userSelectedDate = selectedDates[0];
+          startBtn.disabled = false;
+      }
+  },
+};
+flatpickr(datetimePicker, options);
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    datetimePicker.disabled = true;
+
+    timerId = setInterval(() => {
+        const currentTime = new Date();
+        const deltaTime = userSelectedDate - currentTime;
+        if (deltaTime <= 0) {
+            clearInterval(timerId);
+            updateTimerInterface({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+            return;
+        }
+
+        const time = convertMs(deltaTime);
+        updateTimerInterface(time);
+    }, 1000);
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+function updateTimerInterface({ days, hours, minutes, seconds }) {
+    Ddays.textContent = addLeadingZero(days);
+    Dhours.textContent = addLeadingZero(hours);
+    Dminutes.textContent = addLeadingZero(minutes);
+    Dseconds.textContent = addLeadingZero(seconds);
+}
+
+
+
+
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,50 @@
 console.log('snackbar');
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const { delay, state } = e.currentTarget.elements;
+    const deleyValue = Number(delay.value);
+    const stateValue = state.value;
+    createPromise(deleyValue, stateValue)
+        .then(value => {
+            iziToast.success({
+                title: 'OK',
+                message: `✅ Fulfilled promise in ${value}ms`, position: "topRight"
+            });
+            console.log(`✅ Fulfilled promise in ${value}ms`);
+
+        })
+        .catch(value => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${value}ms`,
+        position: 'topRight'
+            });
+            console.log(`❌ Rejected promise in ${value}ms`);
+        })
+    form.reset();
+});
+
+
+function createPromise(delay , state) {
+    return  new Promise((res, rej) => {
+        setTimeout(() => {
+            if (state ==='fulfilled') {
+                res(delay);
+            } else {
+                rej(delay);
+            }
+        },delay );
+
+    });
+  
+}
